8a28fe5e15fe575ea58cb55bd6f972883e383caa
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-09-10 20:51:42 +0100

    whereami2

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ git diff-tree -r --no-renames --name-only --diff-filter=AM \
 	tee /tmp/templates |
 	sed "s/^/  /" >&2
 
-echo $PWD
+echo $PWD $base
 
 PKGS=$("$base/void-packages/xbps-src" sort-dependencies $(cat /tmp/templates))
 
