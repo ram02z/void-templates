e9c5c34d045d7a8d4a86bb70aee2cb7763c22ad6
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-09-10 21:27:41 +0100

    wh

--- scripts/changed.sh
+++ scripts/changed.sh
@@ -3,6 +3,8 @@
 tip="$(git rev-parse HEAD)"
 base="$(git rev-parse HEAD~)"
 
+git log --stat
+
 /bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
 git diff-tree -r --no-renames --name-only --diff-filter=AM \
 	"$base" "$tip" \
