3f316b2e976c7ad22f82d9e20fb2c0ac7a28176f
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-09-10 21:08:31 +0100

    ci: add changed scipt

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,24 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
 #
 # build.sh
 
-base=$(git rev-parse --show-toplevel)
-
-/bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
-git diff-tree -r --no-renames --name-only --diff-filter=AM \
-	$(git rev-list -1 --parents HEAD) \
-	-- "$base/srcpkgs/*/template" |
-	cut -d/ -f 2 |
-	tee /tmp/templates |
-	sed "s/^/  /" >&2
-
-ls -la $base
-cat $base/srcpkgs/*/template
-
-PKGS=$("$base/void-packages/xbps-src" sort-dependencies $(cat /tmp/templates))
+PKGS=$(./void-packages/xbps-src sort-dependencies $(cat /tmp/templates))
 
 for pkg in ${PKGS}; do
-	"$base/void-packages/xbps-src" -j$(nproc) pkg "$pkg"
+	./void-packages/xbps-src -j$(nproc) pkg "$pkg"
 	[ $? -eq 1 ] && exit 1
 done
 
---
+++ scripts/changed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tip="$(git rev-list -1 --parents HEAD)"
+case "$tip" in
+	*" "*" "*) tip="${tip##* }" ;;
+	*)         tip="${tip%% *}" ;;
+esac
+
+base="$(git merge-base FETCH_HEAD "$tip")"
+
+/bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
+git diff-tree -r --no-renames --name-only --diff-filter=AM \
+	"$base" "$tip" \
+	-- "$base/srcpkgs/*/template" |
+	cut -d/ -f 2 |
+	tee /tmp/templates |
+	sed "s/^/  /" >&2
