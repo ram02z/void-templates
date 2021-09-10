22829a8fd249ebf0d1bdaf4851bb37837f30f673
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-09-10 21:11:38 +0100

    ci: remove old var

--- scripts/changed.sh
+++ scripts/changed.sh
@@ -11,7 +11,7 @@ base="$(git merge-base FETCH_HEAD "$tip")"
 /bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
 git diff-tree -r --no-renames --name-only --diff-filter=AM \
 	"$base" "$tip" \
-	-- "$base/srcpkgs/*/template" |
+	-- "srcpkgs/*/template" |
 	cut -d/ -f 2 |
 	tee /tmp/templates |
 	sed "s/^/  /" >&2
