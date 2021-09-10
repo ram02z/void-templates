69a1e731fcf17c66d29dc96a8dac117a96eb3a63
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-09-10 21:46:35 +0100

    fix(ci): actually export theme

--- scripts/changed.sh
+++ scripts/changed.sh
@@ -3,8 +3,6 @@
 tip="$(git rev-parse HEAD)"
 base="$(git rev-parse HEAD~)"
 
-git log --stat
-
 /bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
 git diff-tree -r --no-renames --name-only --diff-filter=AM \
 	"$base" "$tip" \
@@ -12,3 +10,10 @@ git diff-tree -r --no-renames --name-only --diff-filter=AM \
 	cut -d/ -f 2 |
 	tee /tmp/templates |
 	sed "s/^/  /" >&2
+
+
+PKGS=$(cat /tmp/templates)
+for pkg in ${PKGS}; do
+	mkdir -p "void-packages/srcpkgs/$pkg" && 
+	cp "/srcpkgs/$pkg/template" "void-packages/srcpkgs/$pkg/template"
+done
